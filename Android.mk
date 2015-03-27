edc7efd42da8f2ee5e0158487d47bfd163a72b72
Pierre Zurek <pierre.zurek@parrot.com>
2015-03-27 16:29:57 +0100

    [DEV] Use alchemy and repo
    Change-Id: I7b9c1066d21de8bc053763c69d61acb6aa792a77
    Reviewed-on: https://gerrit.parrot.biz/22600
    Reviewed-by: Yves-Marie Morgan <yves-marie.morgan@parrot.com>
    Reviewed-by: Nicolas Brulez <nicolas.brulez@parrot.com>

---
+++ Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH := $(call my-dir)
+
+# JNI Wrapper
+include $(CLEAR_VARS)
+
+LOCAL_CFLAGS := -g
+ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
+    LOCAL_CFLAGS += -mfloat-abi=softfp -mfpu=neon
+endif
+LOCAL_MODULE := libarmavlink_android
+LOCAL_SRC_FILES := JNI/c/ARMAVLINK_JNIListUtils.c JNI/c/ARMAVLINK_JNIFileParser.c JNI/c/ARMAVLINK_JNIFileGenerator.c JNI/c/ARMAVLINK_JNIMissionItemUtils.c
+LOCAL_LDLIBS := -llog -lz
+LOCAL_SHARED_LIBRARIES := libARMavlink-prebuilt libARSAL-prebuilt
+include $(BUILD_SHARED_LIBRARY)
