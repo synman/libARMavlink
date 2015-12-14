f4cd0d8756f695867ccea098d30c2ad0d98c6465
Djavan Bertrand <djavan.bertrand@parrot.com>
2015-12-14 16:51:42 +0100

    [DEBUG] (iOS) Fix compilation error
    CFlags seems to be overridden by the autotools args
    Also copy mavlink headers to the include folder
    
    Change-Id: I5849d2b8116c4033adc16303c9c6d603cbdbe52b
    Reviewed-on: https://gerrit.parrot.biz/26512
    Reviewed-by: Jean-Baptiste Dubois <jean-baptiste.dubois@parrot.com>

--- atom.mk
+++ atom.mk
@@ -13,6 +13,9 @@ LOCAL_EXPORT_LDLIBS := -larmavlink
 LOCAL_CUSTOM_MACROS := \
 	mavgen-macro:C,generated,$(LOCAL_PATH)/message_definitions/parrot.xml
 
+LOCAL_EXPORT_C_INCLUDES += \
+	$(call local-get-build-dir)/generated
+
 # Copy in build dir so bootstrap files are generated in build dir
 LOCAL_AUTOTOOLS_COPY_TO_BUILD_DIR := 1
 
@@ -33,12 +36,13 @@ LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
 
 else ifneq ($(filter iphoneos iphonesimulator, $(TARGET_OS_FLAVOUR)),)
 
+LOCAL_CFLAGS += -std=gnu99 -x c
+
 LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
 	--enable-static \
 	--disable-shared \
 	OBJCFLAGS=" -x objective-c -fobjc-arc -std=gnu99 $(TARGET_GLOBAL_CFLAGS)" \
-	OBJC="$(TARGET_CC)" \
-	CFLAGS=" -std=gnu99 -x c $(TARGET_GLOBAL_CFLAGS)"
+	OBJC="$(TARGET_CC)"
 
 endif
 
@@ -47,6 +51,10 @@ endif
 # Generates files used by configure
 define LOCAL_AUTOTOOLS_CMD_POST_UNPACK
 	$(Q) cd $(PRIVATE_SRC_DIR)/Build && ./bootstrap
+endef
+
+define LOCAL_AUTOTOOLS_CMD_POST_INSTALL
+    $(Q) cp -R $(TARGET_OUT_BUILD)/libARMavlink/generated/ $(TARGET_OUT_STAGING)/usr/include/
 endef
 
 include $(BUILD_AUTOTOOLS)
