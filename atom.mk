d1f3bb9f05bd8d456bec5122fb4876b9c9f3a104
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2016-01-22 17:24:30 +0100

    [BUILD] Do not use autotools anymore, just alchemy build
    Change-Id: Ia89252b280a38eeee5248c7066ac5e3ba3dacf30
    Reviewed-on: https://gerrit.parrot.biz/28570
    Reviewed-by: Nicolas Christe <nicolas.christe@parrot.com>

--- atom.mk
+++ atom.mk
@@ -2,12 +2,13 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_CATEGORY_PATH := dragon/libs
 LOCAL_MODULE := libARMavlink
 LOCAL_DESCRIPTION := ARSDK Mavlink
+LOCAL_CATEGORY_PATH := dragon/libs
 
-LOCAL_LIBRARIES := ARSDKBuildUtils libARSAL
-LOCAL_EXPORT_LDLIBS := -larmavlink
+LOCAL_MODULE_FILENAME := libarmavlink.so
+
+LOCAL_LIBRARIES := libARSAL
 
 # the two following lines are for generating mavlink C headers
 LOCAL_CUSTOM_MACROS := \
@@ -16,45 +17,30 @@ LOCAL_CUSTOM_MACROS := \
 LOCAL_EXPORT_C_INCLUDES += \
 	$(call local-get-build-dir)/generated
 
-# Copy in build dir so bootstrap files are generated in build dir
-LOCAL_AUTOTOOLS_COPY_TO_BUILD_DIR := 1
+LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH)/Includes \
+	$(LOCAL_PATH)/Sources
 
-# Configure script is not at the root
-LOCAL_AUTOTOOLS_CONFIGURE_SCRIPT := Build/configure
+LOCAL_SRC_FILES := \
+	Sources/ARMAVLINK_FileGenerator.c \
+	Sources/ARMAVLINK_FileParser.c \
+	Sources/ARMAVLINK_ListUtils.c \
+	Sources/ARMAVLINK_Manager.c \
+	Sources/ARMAVLINK_MissionItemUtils.c \
+	gen/Sources/ARMAVLINK_Error.c
 
-# Autotools variable
-LOCAL_AUTOTOOLS_CONFIGURE_ARGS := \
-	--with-libARSALInstallDir="" \
-	--with-mavlinkInstallDir=""
+LOCAL_INSTALL_HEADERS := \
+	Includes/libARMavlink/libARMavlink.h:usr/include/libARMavlink/ \
+	Includes/libARMavlink/ARMAVLINK_Error.h:usr/include/libARMavlink/ \
+	Includes/libARMavlink/ARMAVLINK_FileGenerator.h:usr/include/libARMavlink/ \
+	Includes/libARMavlink/ARMAVLINK_FileParser.h:usr/include/libARMavlink/ \
+	Includes/libARMavlink/ARMAVLINK_ListUtils.h:usr/include/libARMavlink/ \
+	Includes/libARMavlink/ARMAVLINK_Manager.h:usr/include/libARMavlink/ \
+	Includes/libARMavlink/ARMAVLINK_MissionItemUtils.h:usr/include/libARMavlink/
 
-ifeq ("$(TARGET_OS_FLAVOUR)","android")
-
-LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
-	--disable-static \
-	--enable-shared \
-	--disable-so-version
-
-else ifneq ($(filter iphoneos iphonesimulator, $(TARGET_OS_FLAVOUR)),)
-
-LOCAL_CFLAGS += -std=gnu99 -x c
-
-LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
-	--enable-static \
-	--disable-shared \
-	OBJCFLAGS=" -x objective-c -fobjc-arc -std=gnu99 $(TARGET_GLOBAL_CFLAGS)" \
-	OBJC="$(TARGET_CC)"
-
-endif
-
-
-# User define command to be launch before configure step.
-# Generates files used by configure
-define LOCAL_AUTOTOOLS_CMD_POST_UNPACK
-	$(Q) cd $(PRIVATE_SRC_DIR)/Build && ./bootstrap
+# Install generated mavlink headers
+define LOCAL_CMD_PRE_INSTALL
+    $(Q) cp -Raf $(TARGET_OUT_BUILD)/libARMavlink/generated/* $(TARGET_OUT_STAGING)/usr/include/
 endef
 
-define LOCAL_AUTOTOOLS_CMD_POST_INSTALL
-    $(Q) cp -R $(TARGET_OUT_BUILD)/libARMavlink/generated/* $(TARGET_OUT_STAGING)/usr/include/
-endef
-
-include $(BUILD_AUTOTOOLS)
+include $(BUILD_LIBRARY)
